7ede473cd69ba8e9efaa3bcc180b3f3f3b6fdec1
goliatone <burgosemi@gmail.com>
2017-05-15 18:29:07 -0400

    Add command new;

---
+++ commands/generate.js
@@ -0,0 +1,51 @@
+'use strict';
+const extend = require('gextend');
+const Cookiecutter = require('../lib');
+const resolve = require('path').resolve;
+
+class GenerateCommand {
+
+    constructor(options = {}) {
+        extend(this, options);
+    }
+
+    execute(event) {
+        event = extend({}, GenerateCommand.DEFAULTS, event);
+
+        event.source = event.pathSolver(event.source);
+        event.output = event.pathSolver(event.output);
+
+        let o = event.options;
+
+        o.templates = event.pathSolver(o.templates);
+
+        let solver = new Cookiecutter();
+
+        return solver.run({
+            uri: event.source,
+            target: event.output,
+            // alias: 'base-template',
+            dryRun: o.dryRun,
+            cachePath: o.templates,
+        });
+    }
+}
+
+GenerateCommand.DEFAULTS = {
+    output: './output',
+    source: '',
+    pathSolver: resolve,
+    options: {
+        clean: false,
+        dryRun: false,
+        promptFile: 'prompt.js',
+        templates: '~/.core.io/templates'
+    }
+};
+
+module.exports = GenerateCommand;
+// uri: './example',
+// -        dryRun: false,
+// -        alias: 'base-template',
+// -        target: '/tmp/core.io/output/#name#',
+// -        cachePath: '/tmp/core.io/templates',
