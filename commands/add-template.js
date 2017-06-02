841effc809938846a94f19cfd5155127020fb146
goliatone <burgosemi@gmail.com>
2017-06-02 19:17:54 -0400

    Remove path solver for event source since we might want relative paths

--- commands/add-template.js
+++ commands/add-template.js
@@ -2,6 +2,7 @@
 const extend = require('gextend');
 const ProjectTemplate = require('../lib/project-template');
 const resolve = require('path').resolve;
+const untildify = require('untildify');
 
 class AddTemplateCommand {
 
@@ -12,10 +13,9 @@ class AddTemplateCommand {
     execute(event) {
         event = extend({}, AddTemplateCommand.DEFAULTS, event);
 
-        event.source = event.pathSolver(event.source);
-
         let o = event.options;
 
+        o.templates = untildify(o.templates);
         o.templates = event.pathSolver(o.templates);
 
         let solver = new ProjectTemplate({
