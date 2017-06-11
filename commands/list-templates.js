8a3b6bb759b2eea5eb8420a3ac35714fdac1f539
goliatone <burgosemi@gmail.com>
2017-06-10 22:39:38 -0400

    Implement generate command

--- commands/list-templates.js
+++ commands/list-templates.js
@@ -2,6 +2,7 @@
 const extend = require('gextend');
 const ProjectTemplate = require('../lib/project-template');
 const resolve = require('path').resolve;
+const untildify = require('untildify');
 
 class ListTemplatesCommand {
 
@@ -14,13 +15,20 @@ class ListTemplatesCommand {
 
         let o = event.options;
 
+        o.templates = untildify(o.templates);
         o.templates = event.pathSolver(o.templates);
 
         let solver = new ProjectTemplate({
+            logger: this.logger,
             cachePath: o.templates
         });
 
-        return solver.list();
+        return solver.list(o).then((templates)=>{
+            this.logger.info('Templates:');
+            this.logger.info(templates);
+            this.logger.info();
+            return templates;
+        });
     }
 }
 
