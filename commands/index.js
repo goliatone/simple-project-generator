2ff6e31571c3331089a327ae97c772dcd9fe559b
goliatone <burgosemi@gmail.com>
2017-10-29 17:21:38 -0400

    use new attach routine

--- commands/index.js
+++ commands/index.js
@@ -5,9 +5,20 @@ const New = require('./new');
 const Link = require('./link');
 const List = require('./list');
 
-module.exports.attach = function(prog, namespace=false) {
-    Add.attach(prog, namespace);
-    List.attach(prog, namespace);
-    New.attach(prog, namespace);
-    Link.attach(prog, namespace);
+/**
+ * Attach commands to given application context,
+ * if a `namespace` is given then commands will 
+ * be added as sub-commands.
+ */
+module.exports.attach = function $attach(app, namespace=false) {
+    
+    const context = {
+        namespace,
+        prog: app.prog
+    };
+
+    Add.attach(context);
+    List.attach(context);
+    New.attach(context);
+    Link.attach(context);
 };
