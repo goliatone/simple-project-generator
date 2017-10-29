49df3794c3ecf403dc4c61139cd70960134b771e
goliatone <burgosemi@gmail.com>
2017-10-28 23:05:56 -0400

    Update require commands

--- commands/index.js
+++ commands/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const Add = require('./add-template');
-const Generate = require('./generate');
+const New = require('./generate');
 const Link = require('./link-template');
 const List = require('./list-templates');
 
 module.exports.attach = function(prog, namespace=false) {
     Add.attach(prog, namespace);
     List.attach(prog, namespace);
-    Generate.attach(prog, namespace);
+    New.attach(prog, namespace);
     Link.attach(prog, namespace);
 };
